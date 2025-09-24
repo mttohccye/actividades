1bce78d0fcb0d3b8fb595efb810c93fabc26cb12
mttohccye <mttohccye@gmail.com>
2025-09-23 21:13:53 -0500

    Add files via upload

---
+++ pantalla.js
@@ -0,0 +1,74 @@
+// variable to store our intervalID
+	window.onload = function(){
+		let currentIntervalIndex = 0;
+		let intervalId;
+		const oElement = document.getElementById("frame1");
+		//const zElement = document.getElementsByTagName("frameset");
+		var contador;
+		contador = 0;
+
+		/*switch(screen.width){
+			case 
+
+			break;
+		}*/
+
+		const intervals = [
+  			{ func: () => imagen1(), time: 9000 },
+  			{ func: () => imagen2(), time: 9000 },
+  			{ func: () => imagen3(), time: 20000 },
+  			{ func: () => imagen4(), time: 20000 },
+  			{ func: () => imagen5(), time: 15000 }/*,
+  			{ func: () => imagen6(), time: 20000 }*/
+		];
+
+		function ejecutarSiguienteIntervalo() {
+  			const current = intervals[currentIntervalIndex];
+ 		 	current.func();
+  			currentIntervalIndex = (currentIntervalIndex + 1) % intervals.length; // Bucle
+  			intervalId = setTimeout(ejecutarSiguienteIntervalo, current.time);
+  			if (contador == 62) {
+  				location.reload();
+  			}
+  			contador = contador + 1;
+  			console.log(contador);
+		}
+
+		function imagen1(){
+			oElement.src = "mtto1e31d_archivos/sheet002.htm";
+			oElement.style.transform = `scale(${screen.width/1300})`;
+		}
+
+		function imagen2(){
+			oElement.src = "mtto1eayer_archivos/sheet002.htm";
+			oElement.style.transform = `scale(${screen.width/1300})`;
+		}
+
+		function imagen3(){
+			oElement.src = "mtto1e31d_archivos/sheet001.htm";
+			oElement.style.transform = `scale(${screen.width/1186})`;
+		}
+
+		function imagen4(){
+			oElement.src = "mtto1eayer_archivos/sheet001.htm";
+			oElement.style.transform = `scale(${screen.width/1186})`;
+		}
+
+		function imagen5(){
+			oElement.src = "Horarios_archivos/sheet001.htm";
+			oElement.style.transform = `scale(${screen.width/754})`;
+			/*oElement.src = "horarios.png";
+			oElement.style.transform = `scale(${screen.width/900})`;*/
+		}
+
+		/*function imagen6(){
+			oElement.src = "cuadroeventos.png";
+			oElement.style.transform = `scale(${screen.width/970})`;
+		}*/
+
+		// Iniciar la ejecución
+		ejecutarSiguienteIntervalo();
+
+		// Para detenerlo:
+		// clearTimeout(intervalId);
+	}
